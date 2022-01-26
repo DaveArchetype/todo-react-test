6b01c3fd1951324fe11b0318fa221442701012ab
Emanuel-David Bratasanu <davidthearchetype@gmail.com>
2022-01-26 23:40:21 +0000

    Noticed the setupTests.js file, added a small comment there just to mention where I did the testing

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,6 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+// not very sure if I was supposed to put the tests here
+// you can find the tests I wrote in src/tests
