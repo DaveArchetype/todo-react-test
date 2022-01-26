7913da7df7a91afa94a66b0a0b218b8b861fde39
Emanuel-David Bratasanu <davidthearchetype@gmail.com>
2022-01-26 23:16:52 +0000

    Made the App.js to be a functional component instead of class based component.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import Todos from "./components/Todos";
 import "./App.css";
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <h1 className="text-center">ToDoist</h1>
-        <Todos />
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="container">
+      <h1 className="text-center">ToDoist</h1>
+      <Todos />
+    </div>
+  );
 }
